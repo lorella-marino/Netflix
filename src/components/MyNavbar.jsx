0d34cf901392d09b31e6621e203b8e4e1bb9a2d4
lorella-marino <lorellamarino@gmail.com>
2025-09-25 15:14:16 +0200

    logo

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -10,7 +10,7 @@ const MyNavbar = () => {
     <Navbar>
       <Container className="justify-content-start align-items-center">
         <Navbar.Brand href="#">
-          <img src="./src/assets/img/pngegg.png" alt="Logo" width="80" />
+          <img src="./src/assets/img/logo.png" alt="Logo" width="80" />
         </Navbar.Brand>
         <Nav className="d-flex flex-row">
           <Nav.Link href="#" className="fs-6 text-secondary fw-semibold mx-2">
